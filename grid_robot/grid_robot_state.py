acd59480397bb8c756f3dd1f3510d4fbedc4f524
fadiz911 <fadiz@post.bgu.ac.il>
2024-11-24 17:33:50 +0200

    begin to implement base heurestic and implement is_goal_state and some getters in grid_robot_state.py

--- grid_robot/grid_robot_state.py
+++ grid_robot/grid_robot_state.py
@@ -3,11 +3,17 @@ class grid_robot_state:
 
     def __init__(self, robot_location, map=None, lamp_height=-1, lamp_location=(-1, -1)):
         # you can use the init function for several purposes
-        pass
+        self.robot_location = robot_location
+        self.map = map
+        self.lamp_height = lamp_height
+        self.lamp_location = lamp_location
 
     @staticmethod
     def is_goal_state(_grid_robot_state):
-        pass
+        robot_location = _grid_robot_state.robot_location
+        lamp_location = _grid_robot_state.lamp_location
+        return robot_location[0] == lamp_location[0] and robot_location[1] == lamp_location[1] and grid_robot_state.get_lamp_height() == grid_robot_state.get_location_value(lamp_location)
+
 
     def get_neighbors(self):
         pass
@@ -25,3 +31,15 @@ class grid_robot_state:
 
 
     #you can add helper functions
+
+    def get_lamp_location(self):
+        return self.lamp_location
+
+    def get_lamp_height(self):
+        return self.lamp_height
+
+    def get_location_value(self,location):
+        return self.map[location[0]][location[1]]
+
+    def get_robot_location(self):
+        return self.robot_location
--- grid_robot/heuristics.py
+++ grid_robot/heuristics.py
@@ -1,10 +1,16 @@
+import math
+
 from grid_robot_state import grid_robot_state
+
 
 #test
 
 
 def base_heuristic(_grid_robot_state):
-    return 0
+    robot_location = grid_robot_state.get_robot_location()
+    lamp_location = grid_robot_state.get_lamp_location()
+    return math.sqrt((robot_location[0] - lamp_location[0])**2 +(robot_location[1] - lamp_location[1])**2)
+
 
 def advanced_heuristic(_grid_robot_state):
     return 0
