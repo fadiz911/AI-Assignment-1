a381acfd2b3b3f982988a0efad532e4f1df2f473
Hussien2605 <168656898+Hussien2605@users.noreply.github.com>
2024-11-29 14:41:08 +0200

    advanced_heuristic update

--- grid_robot/heuristics.py
+++ grid_robot/heuristics.py
@@ -16,39 +16,35 @@ def advanced_heuristic(_grid_robot_state):
     lamp_height = _grid_robot_state.get_location_value(lamp_location)
 
     carried_stairs = _grid_robot_state.get_carried_stairs()
-
-    # List of all available stairs (location and height)
-    stairs_locations = [
-        (x, y, _grid_robot_state.get_location_value((x, y)))
-        for x in range(len(_grid_robot_state.map))
-        for y in range(len(_grid_robot_state.map[0]))
-        if _grid_robot_state.get_location_value((x, y)) > 0
-    ]
-
-    # Distance to lamp (Manhattan distance)
-    distance_to_lamp = abs(robot_location[0] - lamp_location[0]) + abs(robot_location[1] - lamp_location[1])
-
-    # Remaining height required at the lamp
     remaining_stairs_height = max(0, lamp_height - carried_stairs)
 
-    # Calculate cost to gather sufficient stairs
+    # --- Precompute available stairs ---
     stairs_costs = []
-    for stair in stairs_locations:
-        distance_to_stair = abs(robot_location[0] - stair[0]) + abs(robot_location[1] - stair[1])
-        stairs_costs.append((distance_to_stair + 1, stair[2]))  # +1 for the pickup cost
+    for x in range(len(_grid_robot_state.map)):
+        for y in range(len(_grid_robot_state.map[0])):
+            stair_height = _grid_robot_state.get_location_value((x, y))
+            if stair_height > 0:
+                distance_to_stair = abs(robot_location[0] - x) + abs(robot_location[1] - y)
+                heapq.heappush(stairs_costs, (distance_to_stair + 1, stair_height))  # +1 for pickup cost
 
-    stairs_costs.sort(key=lambda x: x[0])  # Sort by distance
-
+    # --- Calculate gathering cost using a heap ---
     gathering_cost = 0
-    for cost, height in stairs_costs:
-        if remaining_stairs_height <= 0:
-            break
+    while remaining_stairs_height > 0 and stairs_costs:
+        cost, height = heapq.heappop(stairs_costs)
         remaining_stairs_height -= height
         gathering_cost += cost
 
-    # Add penalty for carrying stairs while moving to the lamp
+    # --- Distance to the lamp ---
+    distance_to_lamp = abs(robot_location[0] - lamp_location[0]) + abs(robot_location[1] - lamp_location[1])
+
+    # --- Carrying penalty ---
     carrying_penalty = carried_stairs * distance_to_lamp
 
-    # Total heuristic cost
-    total_cost = gathering_cost + carrying_penalty
+    # --- Reflected adjustment ---
+    horizontal_cost = abs(robot_location[0] - lamp_location[0])
+    vertical_cost = abs(robot_location[1] - lamp_location[1])
+    reflected_cost = min(horizontal_cost, vertical_cost)
+
+    # --- Total heuristic cost ---
+    total_cost = gathering_cost + carrying_penalty + reflected_cost
     return total_cost
